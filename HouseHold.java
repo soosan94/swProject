8136ef1f24e016fe60c347b84499fc972fb58ca2
unknown <djwldls0405@gmail.com>
2017-11-30 02:08:37 +0900

    Add HouseHold class

---
+++ HouseHold.java
@@ -0,0 +1,28 @@
+package sw;
+
+import java.util.Scanner;
+
+public class HouseHold {
+	
+	public void houseHoldMenu(){
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("===가계부===\n1.가계부 작성, 2.가계부 수정, 3.가계부 삭제, 4.가계부 종료\n원하는 메뉴의 번호를 입력하세요.");
+		int menu = scan.nextInt();
+		
+		switch(menu){
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3: 
+			break;
+		case 4:
+			break;
+		default:
+			break;
+		}
+		
+	}
+
+}
