be8eac9b4be3d0c3eed02c6dcde4850775f4bc2c
unknown <djwldls0405@gmail.com>
2017-11-30 15:50:19 +0900

    Add FileEdit class

---
+++ FileEdit.java
@@ -0,0 +1,17 @@
+package sw;
+
+import java.io.File;
+
+public class FileEdit {
+	public boolean save(File file){
+		return true;
+	}
+	
+	public boolean edit(File file){
+		return true;
+	}
+	
+	public boolean delete(File file){
+		return true;
+	}
+}
